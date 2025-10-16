a457cf188f5ec5efb5b72cb43034f6f0215bcdd2
Fozilbek Fayzullayev <fozilbekfayzullayev@example.com>
2025-10-16 17:49:27 +0500

    add about section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
-const Features = () => {
-  return <div>Features</div>;
+const Features = ({ index, title, body }) => {
+  return (
+    <div
+      className={`feature max-w-[400px] w-full relative ${
+        index % 2 === 0
+          ? "border-l-4 border-gold md:border-l-4 md:border-r-0 pl-3"
+          : "border-r-4 border-gold md:border-l-4 md:border-r-0 pr-3 md:pr-0 md:pl-3"
+      }`}
+    >
+      <h2 className="feature-title font-semibold text-3xl mb-3">{title}</h2>
+      <p className="feature-body">{body}</p>
+    </div>
+  );
 };
 
 export default Features;
